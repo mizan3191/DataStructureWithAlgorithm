57924fad111bffdc3ab7ba9f625ed517cebd353c
Mizanur Rahman <mizanurrahaman020195@gmail.com>
2021-12-17 09:48:46 +0600

    BubbleSort

---
+++ DataStructureAndAlgorithms/BubbleSort/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace BubbleSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = new int[] { 10,5,6,12, 3 };
+            var count = 0;
+
+            for (int i = 0; i < number.Length; i++) 
+            { 
+                for(int j = 0; j < number.Length- i - 1; j++)
+                {
+                    count++;
+
+                    if(number[j] > number[j+1])
+                    {
+                        var temp = number[j];
+                        number[j] = number[j+1];
+                        number[j+1] = temp;
+                    }
+                }
+            }
+
+            for(int i = 0; i < number.Length; i++)
+            {
+                Console.WriteLine(number[i]);
+            }
+
+            Console.WriteLine($"Loop count: {count}");
+        }
+    }
+}
---
+++ DataStructureAndAlgorithms/SelectionSort/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SelectionSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = new int[] { 10, 5, 6, 12, 3 };
+            var count = 0;
+
+            
+            for (int i = 0; i < number.Length; i++)
+            {
+                Console.WriteLine(number[i]);
+            }
+
+            Console.WriteLine($"Loop count: {count}");
+        }
+    }
+}
