44e9770c2149cfdc606741d30c1b87f339e0f86b
Mizanur Rahman <mizanurrahaman020195@gmail.com>
2022-01-03 00:35:54 +0600

    LIFO

---
+++ DataStructureAndAlgorithms/Stack/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Stack
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var data = new StackData(10);
+            data.push(1);
+            data.push(2);
+            data.push(3);
+            data.push(10);
+            data.push(13);
+            data.push(22);
+            data.push(33);
+            data.push(44);
+            data.push(55);
+            data.push(66);
+            data.push(43);
+            data.push(53);
+            data.push(63);
+            data.push(73);
+            data.push(83);
+            data.push(94);
+
+            data.pop();
+            data.pop();
+            data.pop();
+            data.pop();
+            data.pop();
+            data.pop();
+
+            data.push(63);
+            data.push(73);
+            data.push(83);
+            data.push(94);
+
+            Console.WriteLine("Program is Closed!");
+        }
+
+        
+    }
+}
---
+++ DataStructureAndAlgorithms/Stack/StackData.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    public class StackData
+    {
+        public int top = 0;
+        public int max = 0;
+        public int[] array = new int[10];
+        public StackData(int size)
+        {
+            max = size;
+
+        }
+        public void push(int item)
+        {
+            if (top > max-1)
+            {
+                Console.WriteLine("Stack is full!");
+            }
+            else
+            {
+                array[top] = item;
+                top = top + 1;
+                Console.WriteLine($"Successful Inserted {item} position: {top - 1}");
+            }
+        }
+
+        public int pop()
+        {
+            if(top > 0)
+            {
+                top = top - 1;
+                Console.WriteLine($"Successful Deleted {array[top]} position: {top - 1}");
+            }
+            else
+            {
+                Console.WriteLine("Stack is empty!");
+            }  
+            return array[top];
+        }
+    }
+}
