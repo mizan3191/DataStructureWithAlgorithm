221004dd8333e160ff0be62dc5e6aa9d0dbaf875
Mizanur Rahman <mizanurrahaman020195@gmail.com>
2022-04-07 00:20:48 +0600

    save

---
+++ DataStructureAndAlgorithms/BuzzersGame/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace BuzzersGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var participantList = new int[10];
+
+            for(int i = 0; i < participantList.Length; i++)
+            {
+                participantList[i] = 0;
+            }
+
+            for(int i = 0; i < 5; i++)
+            {
+                var id = int.Parse(Console.ReadLine());
+                participantList[id-1]++;
+            }
+
+            var maxscore = 0;
+
+            for(int i = 0; i < participantList.Length; i++)
+            {
+                if(participantList[i] > maxscore)
+                {
+                    maxscore = participantList[i];
+                }
+            }
+
+            for(var i = 0; i < participantList.Length; i++)
+            {
+                if(maxscore == participantList[i])
+                {
+                    Console.WriteLine($"Winner id {i+1}");
+                    break;
+                }
+            }
+        }
+    }
+}
